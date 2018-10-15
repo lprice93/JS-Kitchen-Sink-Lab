73294a4ad9ba25379e18b2a590918d1f8147945b
lprice93 <lprice@wilbron.com>
2018-10-15 17:54:18 -0500

    getting somewhere

--- app.js
+++ app.js
@@ -7,14 +7,29 @@ var x = 5;
 var y = 4;
 var z = x + y;
 
+console.log(person.charCodeAt(0));
 // display an alert for any name beginning with a letter after L
-if (name = MNOPQRSTUVWXYZ) {
-    greeting = "Back of line!";
+if (person.charCodeAt(0) === 75) {
+    alert("Back of line!");
 } else {
-    greeting = "Next!"
+    alert("Next!");
 }
 
 // create a function with an alert
-function myFunction() {
-
+function sayHello() {
+    alert("Hello World!")
 }
+sayHello();
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert(`Sorry ${name} you are not old enough to view this page!`);
+    } else {
+        alert("Carry On");
+    }
+}
+checkAge(person, 19);
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
