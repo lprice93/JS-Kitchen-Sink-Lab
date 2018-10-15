0975f6290c98d489e26c3b0da8b5a15f5e734cb9
lprice93 <lprice@wilbron.com>
2018-10-15 16:31:41 -0500

    stuck here

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ var person = "Lindsay"
 
 const countries = 50;
 
+// compute result of adding 5 and 4 and store in appropriate variable
 var x = 5;
 var y = 4;
 var z = x + y;
+
+// display an alert for any name beginning with a letter after L
+if (name = MNOPQRSTUVWXYZ) {
+    greeting = "Back of line!";
+} else {
+    greeting = "Next!"
+}
+
+// create a function with an alert
+function myFunction() {
+
+}
